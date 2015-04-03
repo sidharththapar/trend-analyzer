7cd335ddc446d9de0c7028abd5a5eefba2f11594
Ads7 <ads71993@gmail.com>
2015-04-04 03:51:07 +0530

    Update AppController.js

--- datacontroller/zApproach/AppController.js
+++ datacontroller/zApproach/AppController.js
@@ -15,6 +15,7 @@ function updateZValues( options ) {
     scores.forEach( function( score ) {
 
         if ( score['mean'] ) {
+        	
             score[ 'mean' ] = ( score.total * score.mean + score.count )/( score.total + 1 );
             score[ 'variance' ] = Math.sqrt( ( score.total * score.variance * score.variance + score.count * score.count ) / ( score.total + 1 ) );
             score[ 'total' ]++;
